9392053a1d38a87c61ab6d676556651e187b9738
Trần Duy Thanh <tranduythanh@ssf.vn>
2016-05-30 10:50:31 +0700

    clean

--- qrencode.h
+++ qrencode.h
@@ -11,21 +11,21 @@
 
 #define USEPRECALC
 
-#ifndef USEPRECALC
-// malloc-ed by initframe, free manually
-extern unsigned char *strinbuf; // string iput buffer
-extern unsigned char *qrframe;
-// setup the base frame structure - can be reused
-void initframe(void);
-// free the basic frame malloced structures
-void freeframe(void);
-// these resturn maximum string size to send in
-unsigned initeccsize(unsigned char ecc, unsigned size);
-unsigned initecc(unsigned char level,unsigned char version);
-#else // precalc-ed arrays
+// #ifndef USEPRECALC
+// // malloc-ed by initframe, free manually
+// extern unsigned char *strinbuf; // string iput buffer
+// extern unsigned char *qrframe;
+// // setup the base frame structure - can be reused
+// void initframe(void);
+// // free the basic frame malloced structures
+// void freeframe(void);
+// // these resturn maximum string size to send in
+// unsigned initeccsize(unsigned char ecc, unsigned size);
+// unsigned initecc(unsigned char level,unsigned char version);
+// #else // precalc-ed arrays
 extern unsigned char strinbuf[];
 extern unsigned char qrframe[];
-#endif
+// #endif
 
 extern unsigned char  WD, WDB;
 #include "qrbits.h"
