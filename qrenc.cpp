bdc1e47db55c621811395c7d305d9fc9bfde0d1a
Trần Duy Thanh <tranduythanh@ssf.vn>
2016-05-30 14:22:37 +0700

    add PAD

--- qrenc.cpp
+++ qrenc.cpp
@@ -15,14 +15,25 @@ int qrenc(const char* str) {
     sprintf(t_str,"P1\n%d %d\n", WD, WD);
     Serial.print(t_str);
 
+    // print PAD
+    for (x = 0; x < WD + 2; x++) Serial.print("0 ");
+    Serial.print("\n");
+
     for (y = 0; y < WD; y++) {
+        printf("0 "); // print PAD
+
         for (x = 0; x < WD; x++) {
             sprintf(t_str,"%d ", QRBIT(x,y) );
             Serial.print(t_str);
         }
+        printf("0 "); // print PAD
 
         Serial.println("");
     }
+
+    // print PAD
+    for (x = 0; x < WD + 2; x++) Serial.print("0 ");
+    Serial.print("\n");
 
     return 0;
 }
